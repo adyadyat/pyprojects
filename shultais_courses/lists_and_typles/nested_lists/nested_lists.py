5e146e0e836af490d33058be0a854e7ccd8ebc15
adyadyat <omorbekov.a@gmail.com>
2020-06-12 00:33:42 +0600

    methods_list

---
+++ shultais_courses/lists_and_typles/nested_lists/nested_lists.py
@@ -0,0 +1,13 @@
+matrix = [[1, 2, 3], [4, 5, 6], [7, 8, 9], [10, 11, 12]]
+
+matrix[2][1] = [8.5, 8.7, 8.9]
+
+print(len(matrix[2][1]))
+print(sum(matrix[2][1]))
+print(matrix[2][1][-1])
+print(matrix[1][-1]) # 6
+print(matrix[1]) # [4, 5, 6]
+print(len(matrix))
+print(12 in matrix)
+print(12 in matrix[3])
+print(matrix)
